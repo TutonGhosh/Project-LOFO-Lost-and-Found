10e4396122f7613544be44901887bd51efdc6a76
TutonGhosh <404ghosh@gmail.com>
2026-04-17 13:28:40 +0600

    profile section completed

--- script.js
+++ script.js
@@ -19,13 +19,14 @@ function signUp()
 // Click Sign In Button
 function btnSignIn()
 {
+    const inputName = document.getElementById('input-name');
     const inputEmail = document.getElementById('input-email');
     const inputPass = document.getElementById('input-pass');
 
     const email = inputEmail.value;
     const pass = inputPass.value;
 
-    if(email.includes('@uu.edu.bd') && pass.length >= 6)
+    if(isNaN(inputName.value) && (email.includes('@uu.edu.bd')) && (pass.length >= 6))
     {
         window.location.href = 'home.html';
     }
@@ -60,5 +61,31 @@ function btnSignUp()
 // Profile
 function profile()
 {
-    
+    window.location.href = 'profile.html';
+}
+// Back To Home
+function backToHome()
+{
+    window.location.href = 'home.html';
+}
+// Create Post From Profile
+function createPostFromProfile()
+{
+    localStorage.setItem('postOrigin', 'profile');
+    window.location.href = 'createpost.html';
+}
+// Create Post From Home
+function createPostFromHome()
+{
+    localStorage.setItem('postOrigin', 'home');
+    window.location.href = 'createpost.html';
+}
+// Go Back Button
+function goBackBtn()
+{
+    const origin = localStorage.getItem("postOrigin");
+    if(origin === "profile")
+        window.location.href = "profile.html";
+    else if(origin === "home")
+        window.location.href = 'home.html';
 }
